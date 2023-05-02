422a65e7d17ec6cdf5fbb35d9503f27f3ca9d410
mmostafa1980 <eng_moataz_mostafa@hotmail.com>
2023-05-02 19:22:38 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "F:\\mezo\\pre-master\\AdvancedAI\\project\\uploaded_files"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+# app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    result = ""
+    if request.method == 'POST':
+        file = request.files['img1']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            # return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            result = filename + " uploaded"
+            # return redirect(url_for('download_file', name=filename))
+    return render_template('index.html', result=result)
