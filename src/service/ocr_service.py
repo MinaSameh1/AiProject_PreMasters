dcc50dfe5703d04883806e34409e95cd87941392
MinaSameh1 <minasameh1@gmail.com>
2023-05-13 22:39:14 +0300

    added comment

--- src/service/ocr_service.py
+++ src/service/ocr_service.py
@@ -3,5 +3,11 @@ from PIL import Image
 import pytesseract
 
 def read_image(file):
+    """
+    Reads image and returns text
+    :param file: image file
+    :return: text from image, empty string if no text found
+    :return: boolean value if text is present or not
+    """
     text = pytesseract.image_to_string(Image.open(file))
     return text, len(text) > 0
