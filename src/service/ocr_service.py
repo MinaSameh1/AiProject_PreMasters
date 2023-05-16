6ac3a52281406784d0da8cdc71dff2d4d50a10f4
mmostafa1980 <eng_moataz_mostafa@hotmail.com>
2023-05-16 19:11:57 +0200

    parse string from image

--- src/service/ocr_service.py
+++ src/service/ocr_service.py
@@ -2,6 +2,41 @@
 from PIL import Image
 import pytesseract
 
+import cv2
+import typing
+import numpy as np
+
+from mltu.inferenceModel import OnnxInferenceModel
+from mltu.utils.text_utils import ctc_decoder, get_cer
+import pandas as pd
+from tqdm import tqdm
+from mltu.configs import BaseModelConfigs
+
+
+class ImageToWordModel(OnnxInferenceModel):
+    def __init__(self, char_list: typing.Union[str, list], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.char_list = char_list
+
+    def predict(self, image: np.ndarray):
+        image = cv2.resize(image, self.input_shape[:2][::-1])
+        image_pred = np.expand_dims(image, axis=0).astype(np.float32)
+        preds = self.model.run(None, {self.input_name: image_pred})[0]
+        text = ctc_decoder(preds, self.char_list)[0]
+        return text
+
+
 def read_image(file):
-    text = pytesseract.image_to_string(Image.open(file))
+    configs = BaseModelConfigs.load("Models/202301111911/configs.yaml")
+    model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
+    # df = pd.read_csv("Models/202301111911/val.csv").values.tolist()
+    text = ""
+
+    try:
+        image = cv2.imread(file)
+        text = model.predict(image)
+    except Exception as error:
+        print(f"\nerror happened with image_path:{file}, Error: {error}")
+
+    # text = pytesseract.image_to_string(Image.open(file))
     return text, len(text) > 0
